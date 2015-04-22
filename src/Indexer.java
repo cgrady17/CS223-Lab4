e1d3848a31f87aeec645b0d257b2aad3df29b430
Connor Grady <connor.grady@gradycapital.us>
2015-04-22 16:14:51 -0500

    Initial comment

--- src/Indexer.java
+++ src/Indexer.java
@@ -1,5 +1,9 @@
 /**
- * Created by vx7936 on 4/22/2015.
+ * UWW CS223 | Lab 4
+ * @author Connor Grady
+ * Indexer class reads a text file and builds an index of words found in the file.
+ * Outputs the list of words and the line numbers where they were found.
  */
 public class Indexer {
+
 }
